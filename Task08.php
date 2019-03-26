7b51d9d16a49532e89c43bab64fe2f7714515fff
Sveta Chernokutsatova <svetchalex@gmail.com>
2019-03-26 08:58:07 +0300

    Tasks

---
+++ Task08.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @param $str
+ *
+ * @return string
+ */
+function find_preg($str)
+{
+    $result = 'не верно';
+    $pattern = '/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/';
+    if (preg_match($pattern, $str) === 1) {
+        $result = 'верно';
+    }
+    return $result;
+}
+
+/**
+ * @param $str
+ *
+ * @return string
+ */
+function find_php($str)
+{
+    $result = 'не верно';
+
+    if (filter_var($str,FILTER_VALIDATE_IP)!==false) {
+        $result = 'верно';
+    }
+    return $result;
+}
+
+find_preg('127.0.0.1');
+find_preg('255.255.255.0');
+find_preg('1300.6.7.8');
+find_preg('254.hzf.bar.10');
+find_php('127.0.0.1');
+find_php('255.255.255.0');
+find_php('1300.6.7.8');
+find_php('254.hzf.bar.10');
