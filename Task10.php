0e0a00c452aa7a6b7134b3eb19091e60186d6efc
Sveta Chernokutsatova <svetchalex@gmail.com>
2019-03-26 10:24:27 +0300

    Tasks

--- Task10.php
+++ Task10.php
@@ -24,17 +24,17 @@ function find_php($str)
 
     $result = 'не верно';
 
-    if (is_numeric($str) === true && strpos($str, '0') !== 0 && strlen($str) === 6) {
+    if (ctype_digit($str) === true && strpos($str, '0') !== 0 && strlen($str) === 6) {
         $result = 'верно';
     }
     return $result;
 }
 
-echo find_preg('123456');
-echo find_preg('234567');
-echo find_preg('1234545');
-echo find_preg('012344');
-echo find_php('123456');
-echo find_php('234567');
-echo find_php('1234545');
-echo find_php('012344');
+find_preg('123456');
+find_preg('234567');
+find_preg('1234545');
+find_preg('012344');
+find_php('123456');
+find_php('234567');
+find_php('1234545');
+find_php('012344');
